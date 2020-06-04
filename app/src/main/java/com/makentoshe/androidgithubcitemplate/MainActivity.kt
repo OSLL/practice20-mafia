f595fcde6cddb4c14765aa159b1c1c3e6303c7eb
SamurajKing <kott9radevelop@gmail.com>
2020-06-04 12:36:41 +0300

    FirstDemo

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -2,12 +2,17 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.searching)
-
+        setContentView(R.layout.activity_main)
+        val listener = View.OnClickListener { p0 ->
+           setContentView(R.layout.searching)
+        }
+        join.setOnClickListener(listener)
     }
 }
