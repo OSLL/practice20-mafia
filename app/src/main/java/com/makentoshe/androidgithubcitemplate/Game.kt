9ebdd6f912c9f77d5494db1be5d2b747fa416920
Ilja112358 <kidotl03@gmail.com>
2020-06-05 00:37:01 +0000

    Connect popups & chat; Change size

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/Game.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Game.kt
@@ -7,6 +7,7 @@ import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,64 +19,35 @@ class Game : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game)
         Log.d("game", "Game is here")
-        var cnt = 0
-        var sc = ScrollingMovementMethod()
-        chat.movementMethod = sc
-        btn.setOnClickListener{
-            val message = chat.text.toString() + "Tuturu!" + cnt.toString() + "\n"
-            chat.text = message
-            cnt += 1
-        }
-        val popupMenu = PopupMenu(this, button1)
+
+        history.movementMethod = ScrollingMovementMethod()
+
+        createPopup(button1)
+        createPopup(button2)
+        createPopup(button3)
+        createPopup(button4)
+        createPopup(button5)
+        createPopup(button6)
+    }
+
+    private fun createPopup(btn: Button) {
+        val popupMenu = PopupMenu(this, btn)
         popupMenu.inflate(R.menu.popupmenu)
 
-        val popupMenu2 = PopupMenu(this, button2)
-        popupMenu2.inflate(R.menu.popupmenu)
-
-        val popupMenu3 = PopupMenu(this, button3)
-        popupMenu3.inflate(R.menu.popupmenu)
-
-        val popupMenu4 = PopupMenu(this, button4)
-        popupMenu4.inflate(R.menu.popupmenu)
-
-        val popupMenu5 = PopupMenu(this, button5)
-        popupMenu5.inflate(R.menu.popupmenu)
-
-        val popupMenu6 = PopupMenu(this, button6)
-        popupMenu6.inflate(R.menu.popupmenu)
-
-        button1.setOnClickListener{
+        btn.setOnClickListener{
             popupMenu.show()
-        }
-
-        button2.setOnClickListener{
-            popupMenu2.show()
-        }
-
-        button3.setOnClickListener{
-            popupMenu3.show()
-        }
-
-        button4.setOnClickListener{
-            popupMenu4.show()
-        }
-
-        button5.setOnClickListener{
-            popupMenu5.show()
-        }
-
-        button6.setOnClickListener{
-            popupMenu6.show()
         }
 
         popupMenu.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.menu1 -> {
-                    text_view.text = "PopupMenu 1"
+                    val message = history.text.toString() + "P1 " + btn.id + "\n"
+                    history.text = message
                     true
                 }
                 R.id.menu2 -> {
-                    text_view.text = "PopupMenu 2"
+                    val message = history.text.toString() + "P2 " + btn.id + "\n"
+                    history.text = message
                     true
                 }
                 else -> false
