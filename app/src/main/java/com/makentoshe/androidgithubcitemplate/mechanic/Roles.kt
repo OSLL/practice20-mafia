78c8bb08e2b28a99a43d030085cbf6b57438f135
Denis <dns.aqua.ban@gmail.com>
2020-06-05 15:47:37 +0300

    rename some files

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Roles.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Roles.kt
@@ -1,29 +1,29 @@
 package com.makentoshe.androidgithubcitemplate.mechanic
 
-abstract class Role {
+abstract class Roles {
     abstract val text: String
     abstract val role: String
 
     fun choose(): Int {
-        return 0;
+        return 0
     }
 }
 
-class Simple() : Role() {
+class Simple() : Roles() {
     override val text: String
         get() = "Sleep"
     override val role: String
         get() = "Simple"
 }
 
-class Doctor() : Role() {
+class Doctor() : Roles() {
     override val text: String
         get() = "Choose sbd to heal"
     override val role: String
         get() = "Doctor"
 }
 
-class Mafia() : Role() {
+class Mafia() : Roles() {
     override val text: String
         get() = "Choose sbd to kill"
     override val role: String
