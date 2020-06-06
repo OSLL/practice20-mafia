d1aad490c7ce3b56dd4a02385d972b1b7fec2111
SamurajKing <kott9radevelop@gmail.com>
2020-06-06 14:34:06 +0300

    refactor Player.kt add nightAction and dayAction in BotPlayer

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Players.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Players.kt
@@ -1,21 +1,25 @@
 package com.makentoshe.androidgithubcitemplate.mechanic
 
-open class Player(private var role: Roles, private var id: Int) {
-    open fun nightAction() : Int{
+open class Player(var role: Roles, var id: Int) {
+    open fun nightAction(alives : ArrayList<Int>) : Int{
         return 0
     }
 
-    open fun dayAction() : Int{
+    open fun dayAction(alives : ArrayList<Int>) : Int{
         return 0
     }
 }
 
 class BotPlayer(role: Roles, id: Int): Player(role, id) {
-    override fun nightAction() : Int {
-        return 0
+    override fun nightAction(alives : ArrayList<Int>) : Int {
+        return role.choose(alives, id)
     }
 
-    override fun dayAction() : Int {
-        return 0
+    override fun dayAction(alives : ArrayList<Int>) : Int {
+        alives.remove(id)
+        val left = 0
+        val right = alives.size - 1
+        val myChoose = (left..right).random()
+        return alives[myChoose]
     }
 }
