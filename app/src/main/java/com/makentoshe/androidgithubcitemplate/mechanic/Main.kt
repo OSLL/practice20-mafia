1bd385046ab904f260791656772c10d6f891bd00
Ilja112358 <kidotl03@gmail.com>
2020-06-06 21:09:16 +0000

    Mini upd Main, Table

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Main.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Main.kt
@@ -3,6 +3,6 @@ package com.makentoshe.androidgithubcitemplate.mechanic
 import android.widget.Button
 import android.widget.TextView
 
-class Main(var tv: TextView, val btn1: Button, val btn2: Button, val btn3: Button, val btn4: Button, val btn5: Button, val btn6: Button) {
+class Main(var tv: TextView, val btn0: Button, val btn1: Button, val btn2: Button, val btn3: Button, val btn4: Button, val btn5: Button) {
     
 }
