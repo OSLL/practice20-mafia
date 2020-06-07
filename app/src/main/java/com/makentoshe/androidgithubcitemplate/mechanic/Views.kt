2b0226fe5d903bcb96fe193c8787600e2ab27d2d
Ilja112358 <kidotl03@gmail.com>
2020-06-07 18:20:32 +0000

    Upd views

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Views.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Views.kt
@@ -3,10 +3,25 @@ package com.makentoshe.androidgithubcitemplate.mechanic
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.TextView
+import com.makentoshe.androidgithubcitemplate.R
 
 class Views(private val arrayBtn: Array<Button>, private val arrayPm: ArrayList<PopupMenu>,
             private val icon: TextView, private val state: TextView) {
-    fun deletePopup() {
+    fun setBtnActive(id : Int)
+    {
+        arrayBtn[(id + 5) % 6].setBackgroundResource(R.drawable.circle_button)
+        arrayBtn[id].setBackgroundResource(R.drawable.circle_button_active)
+    }
+
+    fun deletePopups() {
+
+    }
+
+    fun setPopupInactive() {
+
+    }
+
+    fun allPopups() {
 
     }
 }
