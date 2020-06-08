707b92ba53bacfe6df7722f4dfac2ed876552855
SamurajKing <kott9radevelop@gmail.com>
2020-06-09 00:11:31 +0300

    add aqua and skin manage

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Views.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/mechanic/Views.kt
@@ -11,14 +11,17 @@ import com.makentoshe.androidgithubcitemplate.R
 class Views(private val arrayBtn: Array<Button>, private val arrayPm: ArrayList<PopupMenu>,
             private val icon: ImageView, private val state: TextView,
             private val exit: Button, private val start: Button) {
+    var skinUsual = arrayOf(R.drawable.circle_button0, R.drawable.circle_button1, R.drawable.circle_button2, R.drawable.circle_button3, R.drawable.circle_button4, R.drawable.circle_button5)
+    var skinActive = arrayOf(R.drawable.circle_button_active0, R.drawable.circle_button_active1, R.drawable.circle_button_active2, R.drawable.circle_button_active3, R.drawable.circle_button_active4, R.drawable.circle_button_active5)
+    var skinDie = arrayOf(R.drawable.circle_button_die0, R.drawable.circle_button_die1, R.drawable.circle_button_die2, R.drawable.circle_button_die3, R.drawable.circle_button_die4, R.drawable.circle_button_die5)
     fun setBtnActive(id : Int, prevId: Int)
     {
-        arrayBtn[prevId].setBackgroundResource(R.drawable.circle_button)
-        arrayBtn[id].setBackgroundResource(R.drawable.circle_button_active)
+        arrayBtn[prevId].setBackgroundResource(skinUsual[prevId])
+        arrayBtn[id].setBackgroundResource(skinActive[id])
     }
 
     fun setBtnActive(id: Int) {
-        arrayBtn[id].setBackgroundResource(R.drawable.circle_button_active)
+        arrayBtn[id].setBackgroundResource(skinActive[id])
     }
 
     fun blockPopups() {
@@ -49,7 +52,7 @@ class Views(private val arrayBtn: Array<Button>, private val arrayPm: ArrayList<
     }
 
     fun setPlayerDie(id: Int) {
-        arrayBtn[id].setBackgroundResource(R.drawable.circle_button_die)
+        arrayBtn[id].setBackgroundResource(skinDie[id])
         arrayBtn[id].setOnClickListener(null)
     }
 
