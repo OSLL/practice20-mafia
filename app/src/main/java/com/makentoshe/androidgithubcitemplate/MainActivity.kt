8b8553028f64efa7bbfad2d682c9ffad25dc6832
Denis <dns.aqua.ban@gmail.com>
2020-06-13 02:34:53 +0300

    add spinner in activity_main.xml

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -4,10 +4,19 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private var spinner: Spinner? = null
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var itemList = arrayOf("4", "5", "6", "7", "8", "9", "10");
+    private var counterChoose: Int = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +42,19 @@ class MainActivity : AppCompatActivity() {
         instruction.setOnClickListener {
             dialog.show()
         }
+
+        spinner = findViewById(R.id.spinners)
+        arrayAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, itemList)
+        spinner?.adapter = arrayAdapter
+        spinner?.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Toast.makeText(applicationContext, "Nothing select", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var items: String = parent?.getItemAtPosition(position) as String
+        counterChoose = items.toInt()
     }
 }
